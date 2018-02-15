7916232c9749fe285b18b21393542b2aa2153c00
Utku Cuhadaroglu <utku.cuhadaroglu@gmail.com>
2018-02-15 21:10:41 +0300

    New implementation of search function and tests.
    Old search function is obsolute and new advanved search function is implemented.
    New tests are placed for coverage of new string field.
    Indentation is fixed.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,75 +1,90 @@
 package com.mycompany.app;
+
 import java.util.*;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
+public class App {
 
+	public static boolean advSearch(ArrayList<String> array1, ArrayList<String> array2, int e, int k) {
+		if (array1 == null | array2 == null)
+			return false;
+		boolean flag1 = false;
+		boolean flag2 = false;
 
-public class App
-{
-    public static boolean search(ArrayList<String> array, int e ,int k) {
-      System.out.println("inside search");
-      if (array == null) return false;
+		for (int i = 0; i < array1.size(); i++) {	//Search Array 1
+			int tempValue = array1.get(i).length() + 1;
+			if (tempValue >= e && tempValue <= k) {
+				flag1 = true;
+			}
+		}
+		for (int i = 0; i < array2.size(); i++) {	//Search Array 2
+			int tempValue = array2.get(i).length() + 1;
+			if (tempValue >= e && tempValue <= k) {
+				flag2 = true;
+			}
+		}
+		if (flag1 == true && flag2 == true)		//If both Array1 and Array2 contains string in given range return True
+			return true;
 
-      for(int i = 0 ; i<array.size() ; i++){
-        int tempValue = array.get(i).length() +1;
-        if (tempValue >= e && tempValue <= k){
-          return true;
-        }
-      }
-      return false;
-    }
+		return false;	//At least one string field doesn't contain conditions.
+	}
 
-    public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+	public static void main(String[] args) {
+		port(getHerokuAssignedPort());
 
-        get("/", (req, res) -> "Hello, World");
+		get("/", (req, res) -> "Hello, World");
 
-        post("/compute", (req, res) -> {
-          //System.out.println(req.queryParams("input1"));
-          //System.out.println(req.queryParams("input2"));
+		post("/compute", (req, res) -> {
+			// System.out.println(req.queryParams("input1"));
+			// System.out.println(req.queryParams("input2"));
 
-          String input1 = req.queryParams("input1");
-          java.util.Scanner sc1 = new java.util.Scanner(input1);
-          sc1.useDelimiter("\\n");
-          java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext())
-          {
-            String value =sc1.next().replaceAll("\\s","");
-            inputList.add(value);
-          }
-          System.out.println(inputList);
+			String input1 = req.queryParams("input1"); // First Array
+			java.util.Scanner sc1 = new java.util.Scanner(input1);
+			sc1.useDelimiter("\\n");
+			java.util.ArrayList<String> inputList = new java.util.ArrayList<>();
 
+			while (sc1.hasNext()) {
+				String value = sc1.next().replaceAll("\\s", "");
+				inputList.add(value);
+			}
+			sc1.close();
 
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          String input3 = req.queryParams("input3").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
-          int input3AsInt = Integer.parseInt(input3);
+			String input4 = req.queryParams("input4"); // Second Array
+			java.util.Scanner sc2 = new java.util.Scanner(input4);
+			java.util.ArrayList<String> inputList2 = new java.util.ArrayList<>();
+			sc2.useDelimiter("\\n");
+			while (sc2.hasNext()) {
+				String value = sc2.next().replaceAll("\\s", "");
+				inputList2.add(value);
+			}
+			sc2.close();
 
+			String input2 = req.queryParams("input2").replaceAll("\\s", "");
+			String input3 = req.queryParams("input3").replaceAll("\\s", "");
+			int input2AsInt = Integer.parseInt(input2);
+			int input3AsInt = Integer.parseInt(input3);
 
-          boolean result = App.search(inputList, input2AsInt ,input3AsInt);
+			boolean result = App.advSearch(inputList, inputList2, input2AsInt, input3AsInt);
 
-         Map map = new HashMap();
-          map.put("result", result);
-          return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());
+			Map map = new HashMap();
+			map.put("result", result);
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
 
+		get("/compute", (rq, rs) -> {
+			Map map = new HashMap();
+			map.put("result", "not computed yet!");
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
+	}
 
-        get("/compute",
-            (rq, rs) -> {
-              Map map = new HashMap();
-              map.put("result", "not computed yet!");
-              return new ModelAndView(map, "compute.mustache");
-            },
-            new MustacheTemplateEngine());
-    }
-
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+	}
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,51 +8,71 @@ import java.util.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
-      public void testFound() {
-      ArrayList<String> array = new ArrayList<>(Arrays.asList("merhaba","deneme","test","selamlar"));
-      assertTrue(new App().search(array, 2,5));
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 
-    public void testNotFound() {
-      ArrayList<String> array = new ArrayList<>(Arrays.asList("merhaba","deneme","test","selamlar"));
-      assertFalse(new App().search(array,2,4));
-    }
+	public void testFound() {
+		ArrayList<String> array = new ArrayList<>(Arrays.asList("merhaba", "deneme", "Yaz", "selamlar"));
+		ArrayList<String> array2 = new ArrayList<>(Arrays.asList("Hi", "Test", "Summer", "Greetings"));
+		new App();
+		assertTrue(App.advSearch(array, array2, 2, 5));
+	}
 
-    public void testEmptyArray() {
-      ArrayList<String> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1,1));
-    }
+	public void testNotFound() {
+		ArrayList<String> array = new ArrayList<>(Arrays.asList("merhaba", "deneme", "test", "selamlar"));
+		ArrayList<String> array2 = new ArrayList<>(Arrays.asList("Hi", "Test", "Summer", "Greetings"));
 
-    public void testNull() {
-      assertFalse(new App().search(null, 1,1));
-    }
+		new App();
+		assertFalse(App.advSearch(array, array2, 2, 4));
+	}
+
+	public void testEmptyArray() {
+		ArrayList<String> array = new ArrayList<>();
+		ArrayList<String> array2 = new ArrayList<>();
+
+		new App();
+		assertFalse(App.advSearch(array, array2, 1, 1));
+	}
+	public void testOneEmptyArray() {
+		ArrayList<String> array = new ArrayList<>();
+		ArrayList<String> array2 = new ArrayList<>(Arrays.asList("merhaba", "deneme", "test", "selamlar"));
+
+		new App();
+		assertFalse(App.advSearch(array, array2, 1, 1));
+		assertFalse(App.advSearch(array2, array, 1, 1));
+	}
+
+	public void testBothNull() {
+		new App();
+		assertFalse(App.advSearch(null, null, 1, 1));
+	}
+
+	public void testOneNull() {
+		ArrayList<String> array1 = new ArrayList<>(Arrays.asList("Hi", "Test", "Summer", "Greetings"));
+		assertFalse(App.advSearch(array1, null, 1, 1));
+		assertFalse(App.advSearch(null, array1, 1, 1));
+
+	}
 }
